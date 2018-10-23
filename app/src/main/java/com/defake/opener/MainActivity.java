87d946373bc8fea94ae9443717d3965aa2242d7f
Genych Alemsky <>
2018-10-23 23:30:56 +0500

    Perform first task

--- app/src/main/java/com/defake/opener/MainActivity.java
+++ app/src/main/java/com/defake/opener/MainActivity.java
@@ -2,12 +2,18 @@ package com.defake.opener;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(TAG, "On Create method called");
+
+        Toast.makeText(getApplicationContext(),"5 minutes have already passed", Toast.LENGTH_LONG).show();
     }
 }
